66239eac64284690a770d13c774f427da3a1b50d
Глеб Власов <vgg@MacBook-Air-Gleb.local>
2022-04-01 22:33:00 +0300

    init commit

--- target/debug/deps/face_detection-9dcf467b9539f927.d
+++
@@ -1,5 +0,0 @@
-/Users/vgg/Desktop/mtuci/Rust/rs-face-detection/target/debug/deps/face_detection-9dcf467b9539f927: src/main.rs
-
-/Users/vgg/Desktop/mtuci/Rust/rs-face-detection/target/debug/deps/face_detection-9dcf467b9539f927.d: src/main.rs
-
-src/main.rs:
